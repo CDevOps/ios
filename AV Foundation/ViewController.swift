2ce73bfc46643aa123c323a961bc82f04fe19306
CloudRit <Ritesh@riteshs-macbook-pro.local>
2017-10-02 16:18:40 -0400

    Updated with additional camera code

--- AV Foundation/ViewController.swift
+++ AV Foundation/ViewController.swift
@@ -1,13 +1,5 @@
-//
-//  ViewController.swift
-//  AV Foundation
-//
-//  Created by Pranjal Satija on 5/22/17.
-//  Copyright © 2017 Pranjal Satija. All rights reserved.
-//
-
-import Photos
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
     let cameraController = CameraController()
@@ -24,6 +16,51 @@ class ViewController: UIViewController {
     
     ///Allows the user to put the camera in video mode.
     @IBOutlet fileprivate var videoModeButton: UIButton!
+    ///Allows the user to switch cameras
+    @IBAction func switchCameras(_ sender: UIButton) {
+        do {
+            try cameraController.switchCameras()
+        }
+            
+        catch {
+            print(error)
+        }
+        
+        switch cameraController.currentCameraPosition {
+        case .some(.front):
+            toggleCameraButton.setImage(#imageLiteral(resourceName: "Front Camera Icon"), for: .normal)
+            
+        case .some(.rear):
+            toggleCameraButton.setImage(#imageLiteral(resourceName: "Rear Camera Icon"), for: .normal)
+            
+        case .none:
+            return
+        }
+    }
+    ///Allows the user to toggle flash
+    @IBAction func toggleFlash(_ sender: UIButton) {
+        if cameraController.flashMode == .on {
+            cameraController.flashMode = .off
+            toggleFlashButton.setImage(#imageLiteral(resourceName: "Flash Off Icon"), for: .normal)
+        }
+            
+        else {
+            cameraController.flashMode = .on
+            toggleFlashButton.setImage(#imageLiteral(resourceName: "Flash On Icon"), for: .normal)
+        }
+    }
+    @IBAction func captureImage(_ sender: UIButton) {
+        cameraController.captureImage {(image, error) in
+            guard let image = image else {
+                print(error ?? "Image capture error")
+                return
+            }
+            
+            try? PHPhotoLibrary.shared().performChangesAndWait {
+                PHAssetChangeRequest.creationRequestForAsset(from: image)
+            }
+        }
+    }
     
     override var prefersStatusBarHidden: Bool { return true }
 }
@@ -49,50 +86,5 @@ extension ViewController {
         
         styleCaptureButton()
         configureCameraController()
-    }
-}
-
-extension ViewController {
-    @IBAction func captureImage(_ sender: UIButton) {
-        cameraController.captureImage {(image, error) in
-            guard let image = image else { print(error as Any); return }
-
-            try? PHPhotoLibrary.shared().performChangesAndWait {
-                PHAssetChangeRequest.creationRequestForAsset(from: image)
-            }
-        }
-    }
-    
-    @IBAction func toggleFlash(_ sender: UIButton) {
-        if cameraController.flashMode == .on {
-            cameraController.flashMode = .off
-            toggleFlashButton.setImage(#imageLiteral(resourceName: "Flash Off Icon"), for: .normal)
-        }
-        
-        else {
-            cameraController.flashMode = .on
-            toggleFlashButton.setImage(#imageLiteral(resourceName: "Flash On Icon"), for: .normal)
-        }
-    }
-    
-    @IBAction func switchCameras(_ sender: UIButton) {
-        do {
-            try cameraController.switchCameras()
-        }
-        
-        catch {
-            print(error)
-        }
-        
-        switch cameraController.currentCameraPosition {
-        case .some(.front):
-            toggleCameraButton.setImage(#imageLiteral(resourceName: "Front Camera Icon"), for: .normal)
-            
-        case .some(.rear):
-            toggleCameraButton.setImage(#imageLiteral(resourceName: "Rear Camera Icon"), for: .normal)
-            
-        case .none:
-            return
-        }
     }
 }
